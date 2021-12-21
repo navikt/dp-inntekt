e6d9a38364eeb6fc2cb302749f465e20888d683b
Geir André Lund <geir.andre.lund@nav.no>
2021-12-21 15:06:30 +0100

    Fjernet uttrekksjobb     navikt/dagpenger#1012

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Application.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/Application.kt
@@ -127,17 +127,5 @@ fun main() {
                 }
             )
         }
-
-        Uttrekksjobb(dataSource, inntektskomponentHttpClient).also {
-
-            try {
-                LOGGER.info { "Starter Uttrekksjobb " }
-                it.hentInntekterOgSjekk()
-                LOGGER.info { "Uttrekksjobb ferdig" }
-            } catch (e: Exception) {
-                LOGGER.error(e) { "Kunne ikke gjøre Uttrekksjobb" }
-            }
-
-        }
     }
 }
