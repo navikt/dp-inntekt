d7ae7d974d373912357de6718d14c964eb7cbf37
André Roaldseth <andre.roaldseth@nav.no>
2022-04-12 13:02:05 +0200

    Logg host og remoteHost

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/v1/KronetillegUttrekkApi.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/v1/KronetillegUttrekkApi.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.inntekt.v1
 
 import io.ktor.application.call
+import io.ktor.request.authorization
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -12,11 +13,12 @@ import no.nav.dagpenger.inntekt.db.KronetilleggUttrekk
 private val logger = KotlinLogging.logger {}
 
 internal fun Route.uttrekk(kronetilleggUttrekk: KronetilleggUttrekk) {
-
     route("{inntektID}") {
         get("harDagpenger") {
-            logger.info { "Requesten har disse dataene: ${call.request.local}"}
-            val inntektId = InntektId(this.call.parameters["inntektID"] ?: throw IllegalArgumentException("inntektID mangler i parameter"))
+            logger.info { "Requesten har disse dataene: host=${call.request.local.host}, remoteHost=${call.request.local.remoteHost}" }
+            val inntektId = InntektId(
+                this.call.parameters["inntektID"] ?: throw IllegalArgumentException("inntektID mangler i parameter")
+            )
             val uttrekk = kronetilleggUttrekk.utrekkFra(inntektId)
             call.respond(uttrekk)
         }
