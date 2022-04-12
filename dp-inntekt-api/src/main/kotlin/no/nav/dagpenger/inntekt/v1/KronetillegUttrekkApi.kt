44e138f5aa8c3abab11007c08b7f6557aae2a135
André Roaldseth <andre.roaldseth@nav.no>
2022-04-12 12:52:29 +0200

    Logg request kontekst

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/v1/KronetillegUttrekkApi.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/v1/KronetillegUttrekkApi.kt
@@ -5,13 +5,17 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
+import mu.KotlinLogging
 import no.nav.dagpenger.inntekt.db.InntektId
 import no.nav.dagpenger.inntekt.db.KronetilleggUttrekk
+
+private val logger = KotlinLogging.logger {}
 
 internal fun Route.uttrekk(kronetilleggUttrekk: KronetilleggUttrekk) {
 
     route("{inntektID}") {
         get("harDagpenger") {
+            logger.info { "Requesten har disse dataene: ${call.request.local}"}
             val inntektId = InntektId(this.call.parameters["inntektID"] ?: throw IllegalArgumentException("inntektID mangler i parameter"))
             val uttrekk = kronetilleggUttrekk.utrekkFra(inntektId)
             call.respond(uttrekk)
