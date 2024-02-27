41d961543e39eeb625a02eaa3b5936b1b477e48e
André Roaldseth <andre.roaldseth@nav.no>
2024-02-27 11:19:22 +0100

    Bare opprett indeksen om den mangler

--- dp-inntekt-api/src/main/resources/db/migration/V11__Index_inntekt_person.sql
+++ dp-inntekt-api/src/main/resources/db/migration/V11__Index_inntekt_person.sql
@@ -1,2 +1,2 @@
-CREATE INDEX CONCURRENTLY idx_inntekt_person_mapping
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_inntekt_person_mapping
     ON inntekt_v1_person_mapping (aktørid, fnr, kontekstid, konteksttype, beregningsdato, "timestamp" DESC)
