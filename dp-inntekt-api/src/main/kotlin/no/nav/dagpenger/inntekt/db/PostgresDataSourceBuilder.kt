aaf2c035a114dadee8d2af4fdbfd28512901e8d8
André Roaldseth <andre.roaldseth@nav.no>
2024-04-24 13:08:17 +0200

    Revert "Logg hvordan vi prøver å koble til database nå"
    This reverts commit ded66d0b82d9461043557b1576f61bbfd8e18719.
    
    Revert "Logg hvordan vi prøver å koble til database nå"
    
    This reverts commit 6b5c9e4d96d4bd79c6e38bf0785359af67c071d4.

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/db/PostgresDataSourceBuilder.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/db/PostgresDataSourceBuilder.kt
@@ -13,7 +13,6 @@ import org.flywaydb.core.api.output.CleanResult
 import org.flywaydb.core.internal.configuration.ConfigUtils
 
 private val logger = KotlinLogging.logger {}
-private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
 // Understands how to create a data source from environment variables
 internal object PostgresDataSourceBuilder {
@@ -37,8 +36,6 @@ internal object PostgresDataSourceBuilder {
             connectionTimeout = 1000
             initializationFailTimeout = 5000
             maxLifetime = 30001
-        }.also {
-            sikkerlogg.info { "Created datasource ${it.dataSourceClassName} for ${it.dataSourceProperties}" }
         }
     }
 
