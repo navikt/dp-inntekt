79ab92945b8779f74ae4b034fb7204bcfbf8ba1d
Arselan.Sultani <arselan.sultani@nav.no>
2025-08-27 13:43:50 +0200

    Fjerne urelevant data vi ikke trenger angående virksomhet

--- dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/api/v1/models/FullVirksomhetsInformasjon.kt
+++ dp-inntekt-api/src/main/kotlin/no/nav/dagpenger/inntekt/api/v1/models/FullVirksomhetsInformasjon.kt
@@ -3,16 +3,4 @@ package no.nav.dagpenger.inntekt.api.v1.models
 data class FullVirksomhetsInformasjon(
     val organisasjonsnummer: String,
     val navn: String,
-    val postadresse: Adresse,
-    val forretningsadresse: Adresse,
-)
-
-data class Adresse(
-    val land: String,
-    val landkode: String,
-    val postnummer: String,
-    val poststed: String,
-    val adresse: List<String>,
-    val kommune: String,
-    val kommunenummer: String,
 )
